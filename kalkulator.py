47dca5bb223994185cb37f9628a1b6705c44b3db
Łukasz Tobolski <tobi1728@gmail.com>
2024-01-14 11:09:51 +0100

    Dodanie kalkulatora

---
+++ kalkulator.py
@@ -0,0 +1,16 @@
+def add(x, y):
+	return x + y
+
+def substract(x, y):
+	return x - y
+
+def multiply(x, y):
+	return x * y
+
+def divide(x , y):
+	return x / y
+
+print(add(1, 2))
+print(substract(5, 4))
+print(multiply(6, 2))
+print(divide(15, 3))
